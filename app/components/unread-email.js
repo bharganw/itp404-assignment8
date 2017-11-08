8ffe7c549b801fc610b91b2b68bcf2567b009f46
YashB9 <bharganwar.yash@gmail.com>
2017-11-07 20:07:51 -0800

    final

--- app/components/unread-email.js
+++ app/components/unread-email.js
@@ -5,6 +5,7 @@ export default Component.extend({
 	title: 'Sign up for Spring classes!',
 	click() {
 		let readToggle = !this.get('read');
+		this.set('read', true);
 		this.get('clickAction')(readToggle);
 	}
 });
